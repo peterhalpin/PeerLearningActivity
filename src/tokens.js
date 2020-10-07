9df74852b1dac5f4af55019fd4cf8c4c1960258a
Xinzhao Li <szxzlee@gmail.com>
2020-10-07 00:33:35 -0400

    setup map React component with MapBox API

---
+++ src/tokens.js
@@ -0,0 +1,3 @@
+// TODO: tokens for testing only
+// upon production, regenerate new tokens and store tokens on server side
+export const MAPBOX_TOKEN = 'pk.eyJ1IjoiZG9pZmVlbHNhZmUiLCJhIjoiY2theWhzbWIyMGJiZjJxcnhyZXh1ZmRjdCJ9.GDlpVmTJnEgvdJL6MCbywQ';
