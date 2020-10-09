95cd08ed61f0738013b01537c39f85b5625089a6
carolinepar <51763164+carolinepar@users.noreply.github.com>
2020-10-09 01:45:48 -0400

    helpButton: Added help button with tooltip

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { Container } from 'semantic-ui-react';
 import Map from './component/map/Map.js';
+import HelpButton from './component/help/HelpButton.js';
 
 function App() {
   return (
-    <Container>
-      <Map/>
-    </Container>
+    <React.Fragment>
+      <HelpButton />
+      <Container>
+        <Map/>
+      </Container>
+    </React.Fragment>
   );
 }
 
