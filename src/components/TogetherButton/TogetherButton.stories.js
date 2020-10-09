3a209b3b9b33817cb813df1e146b06421176c796
Ruochen Li <liruochenv@gmail.com>
2020-10-09 14:25:59 -0400

    get rid of story

--- src/components/TogetherButton/TogetherButton.stories.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import TogetherButton from './TogetherButton';
-
-storiesOf('TogetherButton', module).add('default', () => <TogetherButton />);
