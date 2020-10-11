9d5f39b9dda1a0a84c8bb14fe55e4157fe576207
Ruochen Li <liruochenv@gmail.com>
2020-10-10 21:52:21 -0400

    togetherJS button finish, can't overlay

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Container } from 'semantic-ui-react';
-import Map from './component/map/Map.js';
+import Map from './components/Map/Map.js';
 import HelpButton from './component/help/HelpButton.js';
+import TogetherButton from './components/TogetherButton/TogetherButton';
 
 function App() {
   return (
     <React.Fragment>
+      <TogetherButton/>
       <HelpButton />
       <Container>
         <Map/>
--- src/components/TogetherButton/TogetherButton.js
+++ src/components/TogetherButton/TogetherButton.js
@@ -2,11 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TogetherButton.module.css';
 
-const TogetherButton = () => (
-  <div className={styles.TogetherButton} data-testid="TogetherButton">
-    TogetherButton Component
-  </div>
-);
+class TogetherButton extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      togetherON: false,
+      text: 'Start Collaborating'
+    }
+}
+
+ToggleButton(){
+  if (this.state.togetherON) {
+    this.setState((currentState) => ({
+      togetherON: !currentState.togetherON,
+    }));
+    this.state.text = 'Start Collaborating';
+  } else {
+    this.setState((currentState) => ({
+      togetherON: !currentState.togetherON,
+    }));
+    this.state.text = 'Done Collaborating';
+  }
+
+  window.TogetherJS(this);
+  return false;
+}
+
+render(){
+    return(
+        <div>
+          <button className="TogetherJSButton" onClick={ () => this.ToggleButton() }>
+            {this.state.text}
+          </button>
+        </div>
+    )
+  }
+}
 
 TogetherButton.propTypes = {};
 
