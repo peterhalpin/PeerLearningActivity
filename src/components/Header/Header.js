79e739f6d8755ad910347e6273977d6bc3df1419
Ruochen Li <liruochenv@gmail.com>
2020-10-11 23:58:14 -0400

    starter file for header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Header.css';
+
+const Header = () => (
+  <div className="Header" data-testid="Header">
+    Header Component
+  </div>
+);
+
+Header.propTypes = {};
+
+Header.defaultProps = {};
+
+export default Header;
