317564035301bb2345c1dc09cd653570439ab349
carolinepar <51763164+carolinepar@users.noreply.github.com>
2020-10-15 20:03:11 -0400

    Delete QAPanel import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Map from './components/Map/Map.js';
 import HelpButton from './components/help/HelpButton.js';
 import RadioButtonPanel from './components/RadioButtonPanel/RadioButtonPanel.js';
 import Slider from './components/Slider/Slider.js';
-import QAPanel from './components/QAPanel/QAPanel.js';
 import DataDisplay from './components/DataDisplay/DataDisplay.js';
 import DataPanels from './components/dataPanels/dataPanels.js';
 import TogetherButton from './components/TogetherButton/TogetherButton.js';
