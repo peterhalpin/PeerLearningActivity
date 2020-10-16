b0a5b2f77b944df1e3d4fedc494c4a00d706c0bd
Xinzhao Li <szxzlee@gmail.com>
2020-10-15 20:48:49 -0400

    adding Map and Data container component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
 import { Container } from 'semantic-ui-react';
-import Map from './components/Map/Map.js';
 import HelpButton from './components/help/HelpButton.js';
-import DataPanels from './components/dataPanels/dataPanels.js';
 import TogetherButton from './components/TogetherButton/TogetherButton.js';
-import { onClickMap } from './map-example.js'; // TODO: replace with export functions from custom components
+import MapAndDataContainer from './components/MapAndDataContainer/MapAndDataContainer.js';
 
 function App() {
   
@@ -14,8 +12,7 @@ function App() {
       <HelpButton />
       <Container>
         <TogetherButton/>
-        <DataPanels/>
-        <Map onClickCallback={onClickMap}/>
+        <MapAndDataContainer/>
       </Container>
     </React.Fragment>
   );
