1295a4a2063cb9a8b876a4ec7fdba1456b123a66
Ruochen Li <liruochenv@gmail.com>
2020-10-21 22:54:05 -0400

    elegent way of collecting peers to sync header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Header.css';
-import { local } from 'd3';
 
 class Header extends React.Component {
 
@@ -14,34 +12,16 @@ class Header extends React.Component {
     }
   }
 
-  init() {
-    if (this.state.numOfUser == 0) {
-      console.log("init!!!!!");
-      var len = document.getElementById("togetherjs-dock-participants").children.length;
-      console.log("number of Users:" + len);
-      this.setState({
-        numOfUser: len
-      });
-      console.log("number of Users:" + this.state.numOfUser);
-      var localUsers = [];
-      for (var i = 0; i < len; i++) {
-        console.log("iiiiii" + i);
-        var element = document.getElementById("togetherjs-dock-participants").children[i];
-        var temp = element.children[0];
-        var info = temp.children[0];
-        console.log(this.state.users.length);
-        console.log(len);
-        if (this.state.users.length < len) {
-          console.log(info);
-          localUsers.push(info.textContent);
-        }
-        console.log("userlist" + localUsers);
-      }
-      this.setState({
-        users: localUsers
-      });
-
-    }
+  loadPeers() {
+    var togetherPeers = window.TogetherJS.require('peers');
+    var myName = togetherPeers.Self.name;
+    var myPeers = togetherPeers.getAllPeers();
+    var names = [];
+    names.push(myName);
+    myPeers.forEach(p => {
+      names.push(p.name);
+    });
+    this.setState({users: names});
   }
 
   endTurn() { 
@@ -52,9 +32,10 @@ class Header extends React.Component {
 
   render() {
     return (
-      <div onClick={() => this.init()} className="ui raised center segment compact Header">
-        <p>It's {this.state.users[this.state.currNum]} turn</p>
-        <button onClick={() => this.endTurn() }>End Turn</button>
+      <div className="ui raised center segment compact Header">
+        <p>It's {this.state.users[this.state.currNum]}'s turn</p>
+        <button className="ui button" onClick={() => this.endTurn() }>End Turn</button>
+        <button className="ui button" onClick={() => this.loadPeers() }>Refresh</button>
       </div>
     );
 
