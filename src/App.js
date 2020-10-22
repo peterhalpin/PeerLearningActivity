39def5a1684721911753a3c1b87cda69e3ecde0d
Ruochen Li <liruochenv@gmail.com>
2020-10-22 14:08:35 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Container } from 'semantic-ui-react';
 import HelpButton from './components/help/HelpButton.js';
-import TogetherButton from './components/TogetherButton/TogetherButton.js';
 import LogTable from './components/logTable/logTable.js';
 import MapAndDataContainer from './components/MapAndDataContainer/MapAndDataContainer.js';
 import Header from './components/Header/Header.js';
