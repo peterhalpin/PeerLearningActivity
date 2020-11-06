7cc81482db5bf6a55ff98f462415d9df4e6dbe02
Xinzhao Li <szxzlee@gmail.com>
2020-11-06 11:45:17 -0500

    test DataDisplay

--- src/components/DataDisplay/DataDisplay.test.js
+++ src/components/DataDisplay/DataDisplay.test.js
@@ -2,22 +2,45 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import DataDisplay from './DataDisplay';
-import { dataObject, renderData } from '../../utils/data.js';
+import { mapIntToDate, dataObject, renderData } from '../../utils/data.js';
 
-let newPromise = new Promise(function(resolve) {
-	resolve(renderData());
-})
+let state = 'Alabama';
+let displayName = 'deaths';
+let dateInt = 75;
+let data= 0;
 describe('<DataDisplay />', () => {
-  beforeAll(async (done) => {
-	renderData();
-	done();
+  beforeAll(() => {
+	dataObject.push({
+		date: "2020-02-04",
+		deaths: "0",
+		id: "1089",
+		infections: "0",
+		location: "Alabama",
+		tests: "0",
+		total_deaths: "0",
+		total_infections: "0",
+		total_tests: "0"
+	});
   });
   test('it should mount', () => {
-    		render(<DataDisplay />);
-    		
-    		const dataDisplay = screen.getByTestId('DataDisplay');
+	let dateKey= mapIntToDate(dateInt);
+	render(<DataDisplay selectedDataType={displayName} selectedState={state} selectedDate={dateInt} currentData={data}/>);
+	
+	const dataDisplay = screen.getByTestId('DataDisplay');
 
-    		expect(dataDisplay).toBeInTheDocument();
-		
+	expect(dataDisplay).toBeInTheDocument();
+	
+  });
+  test('it should display correct value based on input', () => {
+	let dateKey= mapIntToDate(dateInt);
+	render(<DataDisplay selectedDataType={displayName} selectedState={state} selectedDate={dateInt} currentData={data}/>);
+	
+	const dataDisplay = screen.getByTestId('DataDisplay');
+
+	expect(dataDisplay).toHaveTextContent(state);
+    expect(dataDisplay).toHaveTextContent(displayName);
+    expect(dataDisplay).toHaveTextContent(dateKey);
+    expect(dataDisplay).toHaveTextContent(data.toString());
+  	
   });
 });
