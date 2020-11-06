d413f28b1bbafe4645f4ac5acef6860677edf268
Xinzhao Li <szxzlee@gmail.com>
2020-11-06 11:47:12 -0500

    test connection between radio button data display

--- src/components/DataPanels/DataPanels.test.js
+++ src/components/DataPanels/DataPanels.test.js
@@ -3,9 +3,8 @@ import { render, screen, wait, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import DataPanels from './DataPanels';
 import MapAndDataContainer from '../MapAndDataContainer/MapAndDataContainer.js';
-import { getDefaultDateInt, mapIntToDate, renderData, organizedObject } from '../../utils/data.js';
+import { headings, dataObject, renderData, organizedObject } from '../../utils/data.js';
 
-//jest.mock('../../utils/data.js');
 
 jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
   GeolocateControl: jest.fn(),
@@ -17,39 +16,55 @@ jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
   NavigationControl: jest.fn(),
 }));
 
+
 describe('<DataPanels />', () => {
-//  beforeAll(async (done) => {
-//  	await renderData();
-//	done();
-//  });
+  beforeAll(() => {
+	headings.push('deaths');
+	headings.push('tests');
+	dataObject.push({
+		date: "2020-02-04",
+		deaths: "0",
+		id: "1089",
+		infections: "0",
+		location: "Alabama",
+		tests: "0",
+		total_deaths: "0",
+		total_infections: "0",
+		total_tests: "0"
+	});
+	organizedObject['Alabama'] = {
+		'deaths': {
+			'2/4/2020': 0
+		}
+	};
+  	
+  })
   test('it should mount', () => {
-	//render(<DataPanels testEnv="true"/>);
 	render(<MapAndDataContainer testEnv='true'/>);
 	const dataPanels = screen.getByTestId('DataPanels');
 	expect(dataPanels).toBeInTheDocument();
   });
-  test('radio button hould trigger the right data', () => {
-	render(<DataPanels testEnv="true"/>);
-	const testText = 'Deaths';
+  test('radio button should trigger the right data', () => {
+	render(<MapAndDataContainer testEnv='true'/>);
+	const testText = 'deaths';
 	testRadioToDataDisplay(testText, testText);
   });
 });
 
 
 const testRadioToDataDisplay = (buttonName, displayName) => {
-    const radio = screen.getByLabelText(buttonName);
+    const radio = screen.getByDisplayValue(buttonName);
 	const state = 'Alabama';
-	const dateInt = getDefaultDateInt();
-	const dateKey = mapIntToDate(dateInt);
-    fireEvent.click(radio);
+	const dateKey = '2/4/2020';
+	fireEvent.change(radio, { target: { value: buttonName } });
 	
-	const data = getData(state, dateKey, buttonName) 
+	const data = getData(state, dateKey, buttonName); 
 
-	if(data) {
-    	expect(screen.getByTestId("DataDisplay")).toHaveTextContent(data.toString());
-	}
-
+    expect(screen.getByTestId("DataDisplay")).toHaveTextContent(state);
     expect(screen.getByTestId("DataDisplay")).toHaveTextContent(displayName);
+    expect(screen.getByTestId("DataDisplay")).toHaveTextContent(dateKey);
+    expect(screen.getByTestId("DataDisplay")).toHaveTextContent(data.toString());
+
 	
 }
 
@@ -57,10 +72,11 @@ const getData = (state, date, type) => {
     if(organizedObject) {
       let dataForState = organizedObject[state];
       if(dataForState) {
-        let destylizedType = testText.replace(' ', '_');
+        let destylizedType = type.replace(' ', '_');
         let dataForType = dataForState[destylizedType];
         if(dataForType) {
-          let dataForDate = dataForType[dataKey];
+		  console.log(date)
+          let dataForDate = dataForType[date];
 		  return dataForDate;
         }
       }
