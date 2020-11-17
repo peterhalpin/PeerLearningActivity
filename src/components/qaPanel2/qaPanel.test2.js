2e3b542ba793e18660f5bb5a571d06f0bd967230
Ruochen Li <liruochenv@gmail.com>
2020-11-17 17:00:03 -0500

    renaming qaPanel 1/2

--- src/components/qaPanel2/qaPanel.test2.js
+++ src/components/qaPanel2/qaPanel.test2.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import QaPanel from "./QaPanel";
+import QaPanel from "./qaPanel2";
 
 describe("<qaPanel />", () => {
   test("it should mount", () => {
