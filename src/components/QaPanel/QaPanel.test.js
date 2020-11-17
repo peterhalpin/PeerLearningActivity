520fb5280349be3a693cd4c10f12ca9d7f14ac42
Ruochen Li <liruochenv@gmail.com>
2020-11-17 17:03:56 -0500

    fix something that's not auto-updated

--- src/components/QaPanel/QaPanel.test.js
+++ src/components/QaPanel/QaPanel.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import QaPanel from "./qaPanel2";
+import QaPanel from "./QaPanel";
 
 describe("<qaPanel />", () => {
   test("it should mount", () => {
