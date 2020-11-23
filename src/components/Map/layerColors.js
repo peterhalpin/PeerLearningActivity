0758ace74059417b6f426a0e696ccac2817e3903
Xinzhao Li <szxzlee@gmail.com>
2020-11-23 13:53:37 -0500

    match map layer color scale number

--- src/components/Map/layerColors.js
+++ src/components/Map/layerColors.js
@@ -1,14 +1,13 @@
 export const layerColors = {
-  // TODO: change layer scale to the actual number
   // Setting: change color scheme of map here
   mapExample: {
     0: "#F2F12D",
-    10: "#EED322",
-    20: "#E6B71E",
-    50: "#DA9C20",
-    100: "#CA8323",
-    200: "#B86B25",
-    500: "#A25626",
-    1000: "#8B4225",
+    500: "#EED322",
+    2000: "#E6B71E",
+    5000: "#DA9C20",
+    20000: "#CA8323",
+    50000: "#B86B25",
+    80000: "#A25626",
+    100000: "#8B4225",
   },
 };
